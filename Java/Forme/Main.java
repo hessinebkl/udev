109aa8db1608ae63e5d9fcd7ead5a7a2481736d5
Lahoucine Bkl <47284666+hessinebkl@users.noreply.github.com>
2023-07-12 10:51:59 +0100

    Update Main.java

--- Java/Forme/Main.java
+++ Java/Forme/Main.java
@@ -1,1 +1,11 @@
+package Forme;
 
+public class Main {
+    public static void main(String[] args){
+        Cercle Cl = new Cercle(5);
+        Carre Cr = new Carre(3);
+
+        Cl.aire();
+        Cr.aire();
+    }
+}
