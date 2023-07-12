ff136171f40d84635c7ac39414e31ce042691c8a
Lahoucine Bkl <47284666+hessinebkl@users.noreply.github.com>
2023-07-12 10:56:37 +0100

    Update Main.java

--- Java/Voiture/Main.java
+++ Java/Voiture/Main.java
@@ -1,1 +1,12 @@
-
+package Voiture;
+public class Main{
+    public static void main(String[] args){
+        Voiture V1 = new Voiture("Volkswagen","Grey",180,150000);
+        V1.afficherDetails();
+        V1.setMarque("Volkswagen");;
+        V1.setCouleur("Blue");
+        V1.setVitesseMax(130);
+        System.out.println("   ");
+        V1.afficherDetails();
+    }
+}
