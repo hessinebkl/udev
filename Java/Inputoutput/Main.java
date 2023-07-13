8589c1327e5820ba8a6585373857b60ab15615b3
Lahoucine Bkl <47284666+hessinebkl@users.noreply.github.com>
2023-07-13 09:56:26 +0100

    Create Main.java

---
+++ Java/Inputoutput/Main.java
@@ -0,0 +1,27 @@
+package Inputoutput;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args){
+        try {
+            FileWriter writer = new FileWriter("lahoucine.txt");
+            writer.write("Hello, my aname is Lahoucine");
+            writer.close();
+
+            FileReader reader = new FileReader("lahoucine.txt");
+            int text;
+            while((text = reader.read()) != -1) {
+                System.out.print((char) text);
+
+
+            }
+            reader.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
