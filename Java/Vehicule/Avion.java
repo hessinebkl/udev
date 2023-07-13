4e35a29af87e21a483f39be5e0667de6cf5b4953
Lahoucine Bkl <47284666+hessinebkl@users.noreply.github.com>
2023-07-13 09:43:43 +0100

    Create Avion.java

---
+++ Java/Vehicule/Avion.java
@@ -0,0 +1,11 @@
+package Vehicule;
+
+public class Avion extends Vehicule implements Volant {
+
+    @Override
+    public void Voler() {
+        System.out.println("l'avion vole");
+
+    }
+    
+}
