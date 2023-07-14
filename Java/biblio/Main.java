22cbe4b84f9c92d0434ecb8798a8685c5eda21bf
Lahoucine Bkl <47284666+hessinebkl@users.noreply.github.com>
2023-07-14 12:17:11 +0100

    Update Main.java

--- Java/biblio/Main.java
+++ Java/biblio/Main.java
@@ -2,13 +2,14 @@ package biblio;
 
 public class Main {
     public static void main(String[] args){
+
         Adherent A1 = new Adherent("lahoucin", "fes", "0625302955");
         Adherent A2 = new Adherent("otman", "fes", "0623651478");
         Adherent A3 = new Adherent("issam", "fes", "0625987896");
 
         Livre L1 = new Livre("rich dad poor dad", "Robert Kiyosaki", 1997, false);
         Livre L2 = new Livre("Rage", "Richard Bachman", 1997, false);
-        Livre L3 = new Livre("It Ends zith Us", "Colleen Hoover", 2020, false);
+        Livre L3 = new Livre("It Ends with Us", "Colleen Hoover", 2020, false);
         Livre L4 = new Livre("The robe", "Lloyd Cassel Douglas", 1999, false);
         Livre L5 = new Livre("Shadows", "C. J. Box", 2022, false);
 
@@ -21,11 +22,10 @@ public class Main {
         B.ajouterLivre(L5);
 
         A1.emprunterLivre(L5);
-        A2.emprunterLivre(L1);
-        A3.emprunterLivre(L3);
+        A2.emprunterLivre(L5);
 
-        L5.imprimer();
-        A1.retournerLivre(L5);
+
+
 
     }
 }
