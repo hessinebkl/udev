9cba5559817c3d442f721de741a240bc82893b4c
Lahoucine Bkl <47284666+hessinebkl@users.noreply.github.com>
2023-07-14 10:59:16 +0100

    Create LivreAudio.java

---
+++ Java/biblio/LivreAudio.java
@@ -0,0 +1,21 @@
+package biblio;
+
+public class LivreAudio extends Livre{
+    private int duree;
+    LivreAudio(String titre, String auteur, int anneeDePublication, boolean estEmprunte,int duree) {
+        super(titre, auteur, anneeDePublication, estEmprunte);
+        this.duree=duree;
+    }
+    
+    // SETTERS
+    public void setDuree(int duree){
+        this.duree=duree;
+    }
+    //GETTERS
+    public int getDuree(){
+        return this.duree;
+    }
+
+
+    
+}
