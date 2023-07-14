0d57b5c24df605f1566c5d0f83af5c71dfa4ce26
Lahoucine Bkl <47284666+hessinebkl@users.noreply.github.com>
2023-07-14 11:13:04 +0100

    Create Test.java

---
+++ Java/Throw/Test.java
@@ -0,0 +1,23 @@
+package Throw;
+
+public class Test {
+    public void checkAge(int age){
+        if(age<18){
+            throw new ArithmeticException("Accés refusé - Vous devez avoir au moins 18 ans.");
+        }
+        else{
+        System.out.println("Accés accordé - Vous avez l'age requis.");
+        }
+    }
+
+    public static void main(String[] args){
+        Test test = new Test();
+        try {
+            test.checkAge(17);
+        } catch (ArithmeticException e) {
+             System.out.println("Exception capturée: "+e.getMessage());
+        } finally{
+             System.out.println("Le bloc finnaly est exécuté.");
+        }
+    }
+}
