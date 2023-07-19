b2930e3257f91d6ec31946a710d18af5dff85c71
Lahoucine Bkl <47284666+hessinebkl@users.noreply.github.com>
2023-07-19 16:00:12 +0100

    Update Bibliotheque.java

--- Java/biblio/Bibliotheque.java
+++ Java/biblio/Bibliotheque.java
@@ -1,16 +1,21 @@
-package biblio;
+package Biblio;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Bibliotheque {
-    private List<String> livres = new ArrayList<String>();
+
+    private List<Livre> livres = new ArrayList<Livre>();
+
+    public void getBiblioLivresNumber() {
+        System.out.println(livres.size());
+    }
 
     public void ajouterLivre(Livre livre){
-        livres.add(livre.getTitre());
+        livres.add(livre);
     }
 
     public void supprimerLivre(Livre livre){
-        livres.remove(livre.getTitre());
+        livres.remove(livre);
     }
 }
